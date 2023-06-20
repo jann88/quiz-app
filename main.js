db13227070eb5a2ab558d6fda59760fbc034d86a
jann88 <jannylynemiaz@gmail.com>
2023-06-20 14:02:08 +0300

    function to load all questions on answer submit

--- main.js
+++ main.js
@@ -39,4 +39,44 @@ const quizData = [
     d: "Kotlin",
     correct: "c",
   },
+  {
+    question: "What year was Javascript launched",
+    a: "2000",
+    b: "1998",
+    c: "2005",
+    d: "1994",
+    correct: "d",
+  },
 ];
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  //select the entire quiz container
+  questionEl.innerText = currentQuizData?.question;
+  a_text.innerText = currentQuizData?.a;
+  b_text.innerText = currentQuizData?.b;
+  c_text.innerText = currentQuizData?.c;
+  d_text.innerText = currentQuizData?.d;
+
+  currentQuiz++;
+}
+
+submitBtn.addEventListener("click", (e) => {
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+    //   currentQuiz++ not necessary
+  } else {
+    alert("You finished! Well done.");
+  }
+});
